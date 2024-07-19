04838f6129d272af940d3ccd109f525128a6d446
qurama9 <rabaidulla@gmail.com>
2024-07-19 21:49:42 +0500

    Initial Commit

---
+++ Challenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenger
+//
+//  Created by Рамазан Абайдулла on 19.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
